ff45bff8e98135683bf7a755d96b4b7fb8dc050f
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-20 14:49:54 +0900

    feat: Add Controls component for managing topic actions

---
+++ src/app/Controls.js
@@ -0,0 +1,39 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
+
+export default function Controls() {
+  const { id } = useParams();
+  const router = useRouter();
+  const handelDelete = () => {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      fetch(`http://localhost:9999/topics/${id}`, { method: "DELETE" })
+        .then(res => res.json())
+        .then(result => {
+          router.push(`/`);
+          router.refresh();
+        });
+    }
+  };
+
+  return (
+    <div className="d-flex gap-1">
+      <Link className="btn btn-primary" href="/create">
+        Create
+      </Link>
+
+      {id && (
+        <>
+          <Link className="btn btn-secondary" href={`/update/${id}`}>
+            Update
+          </Link>
+
+          <button type="button" className="btn btn-danger" onClick={handelDelete}>
+            Delete
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
