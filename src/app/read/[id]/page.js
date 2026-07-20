961011f465ef59a914c69c6c17c931357a45110f
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-20 15:40:23 +0900

    fix: Move parameter destructuring for clarity in Read component

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,7 +1,8 @@
 export default async function Read(props) {
+  const { id } = await props.params;
+
   console.log("Read Page 작동");
 
-  const { id } = await props.params;
   const response = await fetch(`http://localhost:9999/topics/${id}`);
   const topics = await response.json();
 
