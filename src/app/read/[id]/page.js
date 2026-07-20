3fc9febc21b9635809179c80eb428dabad947fcd
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-20 16:01:51 +0900

    fix: Remove unnecessary whitespace in page.js

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,15 +1,11 @@
 export default async function Read(props) {
   const { id } = await props.params;
-
   console.log("Read Page 작동");
-
   const response = await fetch(`http://localhost:9999/topics/${id}`);
   const topics = await response.json();
-
   return (
     <>
       <h2>{topics.title}</h2>
-
       <p>{topics.message}</p>
     </>
   );
