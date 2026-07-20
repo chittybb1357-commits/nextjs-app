07db41af230c799eb361783c6eb465a85d097063
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-20 11:25:17 +0900

    Fix: Update Read component to display topic title and message dynamically

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -2,12 +2,14 @@ export default async function Read(props) {
   console.log("Read Page 작동");
 
   const { id } = await props.params;
+  const response = await fetch(`http://localhost:9999/topics/${id}`);
+  const topics = await response.json();
 
   return (
     <>
-      <h2>Read</h2>
+      <h2>{topics.title}</h2>
 
-      <p>parameter:{id}</p>
+      <p>{topics.message}</p>
     </>
   );
 }
