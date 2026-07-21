94c023713b4702c4f6c346087540fd38962e9052
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-21 11:09:50 +0900

    fix: Remove unnecessary whitespace in page.js for cleaner code

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,15 +1,11 @@
 export default async function Read(props) {
   const { id } = await props.params;
-
   console.log("Read Page 작동");
-
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/topics/${id}`);
   const topics = await response.json();
-
   return (
     <>
       <h2>{topics.title}</h2>
-
       <p>{topics.message}</p>
     </>
   );
